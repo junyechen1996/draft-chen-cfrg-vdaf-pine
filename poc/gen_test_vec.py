7ec264947ca0b50c262e47c2e5c5ca7e9229ccda
Junye Chen <junyec@apple.com>
2024-08-27 05:01:02 -0700

    Generate more test vectors
    Test dimension not a even number, and num_wr_checks not an even number

--- poc/gen_test_vec.py
+++ poc/gen_test_vec.py
@@ -2,7 +2,7 @@ import os
 
 from vdaf_poc.test_utils import gen_test_vec_for_vdaf
 
-from flp_pine import encode_float
+from flp_pine import NUM_WR_CHECKS, NUM_WR_SUCCESSES, encode_float
 from vdaf_pine import Pine64, Pine128
 
 VERSION = int(open('VERSION').read())
@@ -10,47 +10,58 @@ TEST_VECTOR_PATH = os.environ.get('TEST_VECTOR_PATH',
                                   'test_vec/{:02}'.format(VERSION))
 
 if __name__ == '__main__':
-    num_frac_bits = 4
-    l2_norm_bound = encode_float(1.0, num_frac_bits)
-    dimension = 20
+    num_shares = 2
     chunk_length = 150
     chunk_length_norm_equality = 4
-    num_shares = 2
 
-    measurements = []
-    measurements.append([0.0] * dimension)
-    measurements.append([0.0] * dimension)
-    measurements[0][0] = 1.0
-    measurements[1][1] = 1.0
+    for (i, (num_frac_bits,
+             dimension,
+             num_wr_checks,
+             num_wr_successes)) in enumerate([
+                 (7, 20, NUM_WR_CHECKS, NUM_WR_SUCCESSES),
+                 (4, 250, NUM_WR_CHECKS, NUM_WR_SUCCESSES),
+                 (10, 19, 75, 75),
+                 (10, 19, 75, 50)
+             ]):
+        l2_norm_bound = encode_float(1.0, num_frac_bits)
+        measurements = []
+        measurements.append([0.0] * dimension)
+        measurements.append([0.0] * dimension)
+        measurements[0][0] = 1.0
+        measurements[1][1] = 1.0
 
-    pine64 = Pine64(
-        l2_norm_bound,
-        num_frac_bits,
-        dimension,
-        chunk_length,
-        chunk_length_norm_equality,
-        num_shares,
-    )
-    gen_test_vec_for_vdaf(
-        TEST_VECTOR_PATH,
-        pine64,
-        None,
-        measurements,
-        0,
-    )
+        pine64 = Pine64(
+            l2_norm_bound,
+            num_frac_bits,
+            dimension,
+            chunk_length,
+            chunk_length_norm_equality,
+            num_shares,
+            num_wr_checks=num_wr_checks,
+            num_wr_successes=num_wr_successes,
+        )
+        gen_test_vec_for_vdaf(
+            TEST_VECTOR_PATH,
+            pine64,
+            None,
+            measurements,
+            i,
+        )
 
-    pine128 = Pine128(
-        l2_norm_bound,
-        num_frac_bits,
-        dimension,
-        chunk_length,
-        chunk_length_norm_equality,
-        num_shares,
-    )
-    gen_test_vec_for_vdaf(
-        TEST_VECTOR_PATH,
-        pine128,
-        None,
-        measurements,
-        0,
-    )
+        pine128 = Pine128(
+            l2_norm_bound,
+            num_frac_bits,
+            dimension,
+            chunk_length,
+            chunk_length_norm_equality,
+            num_shares,
+            num_wr_checks=num_wr_checks,
+            num_wr_successes=num_wr_successes,
+        )
+        gen_test_vec_for_vdaf(
+            TEST_VECTOR_PATH,
+            pine128,
+            None,
+            measurements,
+            i,
+        )
